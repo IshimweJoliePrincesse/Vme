0ef952d00c9fb5338500a2e411157c19cd81d6ef
IshimweJoliePrincesse <148842543+IshimweJoliePrincesse@users.noreply.github.com>
2024-11-29 14:21:26 +0200

    Contact seection added

--- lib/view/contact_screen.dart
+++ lib/view/contact_screen.dart
@@ -51,14 +51,107 @@ class _ContactScreenState extends State<ContactScreen> {
                 child: Container(
                   decoration: BoxDecoration(
                     color: Provider.of<ThemeModeProvider>(context).themeMode == ThemeMode.light ? whiteColor: const Color(0xFF212121),
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circulat(30),
+                      topLeft: Radius.circular(30))),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children:[
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 10, bottom: 5, left: 8, right: 8
+                          ),
+                          child: Text(
+                            AppLocalizations.of(context)!.how_we_help_you,
+                            style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500
+                            ),
+                          )
+                        ),
+                        LabelInputField(
+                          controller: emailController,
+                          label: AppLocalizations.of(context)!.email,
+                          placeholder: AppLocalizations.of(context)!.enterEmail,
+                          maxLines: 1,
+                          icon: const Icon(Icons.email),
+                          validator: (value){
+                            if(valur!.isEmpty){
+                              return "Email Required";
+                            }
+                            if(!RegExp(emailRegExp).hasMatch(value)){
+                              return "Invalid Email";
+                            }
+                          },
+                        ),
+
+                        LabelInputField(
+                          controller: subjectController,
+                          label: AppLocalizations.of(context)!.subject,
+                          placeholder: AppLocalizations.of(context)!.enterSubject,
+                          maxLines: 1,
+                          icon: const Icon(Icons.subject),
+                          validator: (value){
+                            if(value!.isEmpty){
+                              return "Subject Required";
+                            }
+                          }
+                        ),
+
+                        LabelInputField(
+                          controller: messageController,
+                          label: AppLocalizations.of(context)!.message,
+                          placeholder: AppLocalizations.of(context)!.enterMessage,
+                          maxLines: 4,
+                          icon: const Icon(Icons.message),
+                          validator: (value){
+                            if(value!.isEmpty){
+                              return "Message Required";
+                            }
+                          }
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Button(
+                            tap: () {
+
+                              if(emailController.text == '' || subjectController.text=='' || messageController.text==''){
+                                showSnackBar(context: context, title: "All fields required");
+                              } else {
+                                contactProvider.postContact(
+                                  context, emailController.text.trim(),
+                                  subjectController.text,
+                                  messageController.text
+                                );
+                              }
+                            },
+                            text: AppLocalizations.of(context)!.send,
+                            color: primaryColor,
+                            borderRadius: 10.0,
+                            fontColor: whiteColor,
+                            fontSize: 18
+                          ),
+                        ),
+
+                        if(contactProvider.loading)
+                        Center(child: CupertinoActivityIndicator())
+
+
+                      ],
+                    ),
+                  ),
+                    ),
                     
-                  )
-                )
-              )
-            )
-          ]
-        )
-      )
-    )
+                  ),
+                ),
+          ],
+              ),
+            ),
+          
+        ),
+      
+    
   }
 }
