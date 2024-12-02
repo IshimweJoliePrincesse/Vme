508bcec90a86bcad90c0ed217dea604768e8e1e9
IshimweJoliePrincesse <148842543+IshimweJoliePrincesse@users.noreply.github.com>
2024-12-02 15:19:21 +0200

    Dashboard continuation

--- lib/view/dashboard.dart
+++ lib/view/dashboard.dart
@@ -39,4 +39,170 @@ class _DashboardState extends State<Dashboard> {
     String status = 'all';
     int statusIndex = 0;
     int selectedStatusIndex = 0;
+
+
+
+    Future<void>getById()async {
+
+      try{
+
+        WidgetsFlutterBinding.ensureInitialized();
+
+        SharedPreferences sp= await sharedPreferences.getInstance();
+        final id await sp.getString('voterId') ?? '';
+        print(id);
+        final response = await http.get(Uri.parse('${androidURL}voter/${id}'));
+
+        if(response.statusCode == 200){
+          user = jsonDecode(response.body)['voter'];
+
+          setState((){
+
+          });
+        } 
+        else {
+          debugPrint(response.statusCode.toString());
+        }
+      } catch(e){
+        debugPrint(e.toString());
+      }
+    }
+
+    String formatDate(date){
+      DateTime dateTime = DateTime.parse(date);
+      DateTime dateTimeInRwanda = dateTime.add(Duration(hours: 2));
+
+      String formattedDate = DateFormat('dd mm yyyy').format(dateTimeInRwanda);
+      return formattedDate;
+    }
+
+    String formatTime(date){
+      DateTime dateTime = DateTime.parse(date);
+      DateTime dateTimeInRwanda = dateTime.add(Duration(hours: 2));
+      String formattedTime = DateFormat().add_jm().format(dateTimeInRwanda);
+      return formattedTime;
+    }
+
+    @override
+
+    void initState(){
+      getById();
+      super.initState();
+    }
+
+    @override
+
+   Widget build (BuildContext context){
+
+    final electionProvider = Provider.of<ElectionProvider>(context);
+    final announcementProvider = Provider.of<AnnouncementProvider>(context);
+
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+
+                        Row(
+                          children: [
+
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(color:const Color(0XFFf0f2f5),
+                                borderRadius: BorderRadius.circular(50)
+                              ),
+                            child: ClipRect(
+
+                              borderRadius: BorderRadius.circular(50),
+                              child: user !=null ? Image.network(user['photo'], fit: BoxFit.fitWidth,):Image.asset('assets/images/user.png'),
+                              //FadeInImage.assetNetwork(placeholderFit: BoxFit.scaleDown, fit: BoxFit.cover, placeholder: 'assets/images/loading_gif', image: user !=null ? user['photo']: '' //CupertinoActivityIndicator())
+                            )
+                            ),
+
+                            const SizedBox(width: 7,),
+                            Text("Hi! ${user != null ? user['name']: 'loading...'}", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),),
+                          ],
+                        ),
+
+                        InkWell(
+                          onTap: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=>const SettingScreen()));
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: primaryColoe,
+                              color: const Color(0XFFf0f2f5),
+
+                              borderRadius: BorderRadius,circular(10)
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.all(12.0),
+                              child: Icon(Icons.settings, color: Colors.white,),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+
+                    const SizedBox(height: 20,),
+
+                    CarouselSlider(
+                      options: CarouselOptions(height: 120.0, enlargeCenterPage: true, enlargeFactor: 0.3, scrollDirection: Axis.horizontal, initialPage: 1, autoPlay: true, autoPlayCurve: Curves.fastOutSlowIn, autoPlayAnimation: Duration(seconds: 2), autoPlayInterval: Duration(seconds: 6)),
+                      items: [{'id':'Results', 'color': Colors.deepPurple, 'title': AppLocalizations.of(context)!.results, 'icon': Icons.bar_chart_outlined}, {'id': 'Election', 'color': Colors.blueAccent, 'title': AppLocalizations.of(context)!.elections, 'icon':Icons.how_to_vote_outlined }, {'id': 'Ongoing', 'color': Colors.deepPurpleAccent, 'title':AppLocalizations.of(context)!.ongoing_events, 'icon': Icons.poll_sharp}, {'id': 'Announcements', 'color': Colors.greenAccent, 'title': AppLocalizations.of(context)!.announcements, 'icon': Icons.announcement_outlined}, {'id': 'Parties', 'color': Colors.orange, 'title':AppLocalizations.of(context)!.official_parties, 'icon':Icons.people_alt_sharp}].map((i){
+                        return Builder(
+                          builder: (BuildContext context){
+                            return InkWell(
+                              onTap: (){
+
+                                switch(i['id']){
+                                  case 'Results':
+                                   Navigator.push(context, MaterialPageRoute(builder: (context)=>const ResultsScreen()));
+                                   break;
+                                  case 'Elections':
+                                   Navigator.push(context, MaterialPageRoute(builder: (context)=>const UpcomingEventScreen()));
+                                   break;
+                                  case 'Ongoing':
+                                   Navigator.push(context, MaterialPageRoute(builder: (context)=>const CurrentEventsScreen()));
+                                   break;
+                                  case: 'Announcements':
+                                   Navigator.push(context, MaterialPageRoute(builder: (context)=>const NewsAnnouncementsScreen()));
+                                   break;
+                                  case 'Parties':
+                                   Navigator.push(context, MaterialPageRoute(builder: (context)=>const PoliticalPartiesScreen()));
+                                   break;
+
+                                }
+
+
+                              },
+                            )
+                          }
+                        )
+                      })
+                    )
+                  ]
+                )
+              )
+            ]
+          )
+        )
+      )
+    )
+   }
 }
