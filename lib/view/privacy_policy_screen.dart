cd2e100d4b4197373cf44c88efc0eaca216bbe42
IshimweJoliePrincesse <148842543+IshimweJoliePrincesse@users.noreply.github.com>
2025-01-11 08:53:54 +0200

    new screens

---
+++ lib/view/privacy_policy_screen.dart
@@ -0,0 +1,53 @@
+import 'package :flutter/material.dart';
+import 'package:electa/components/custom_app_bar.dart';
+import 'package:electa/utils/app_colors.dart';
+
+class PrivacyPolicyScreen extends StatelessWidget {
+  const PrivacyPolicyScreen({super.key});
+
+  @override
+
+  Widget build(BuildContext context){
+    return Scaffold(
+
+    appBar: customAppBar(title: "Privacy Policy", context: context),
+    body: SingleChildScrollView(
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Privacy Policy", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),),
+              Text("Ishimwe Jolie Princesse built the ELECTA app as a study project. This service is provided at no cost and is intended for use as is. This page is used to inform visitors regarding our policies with the use and disclosure pf personal information if anyone decided to use my service. If you choose to use my service, then you agree to the collection and use of information in relation to this policy. The personal information that I collect is used for providing and improving the service. I will not use or share your information with anyone except as described in this provacy policy. The terms used in this privacy policy have the same meaning as in our terms and conditions, which are accessible at electa unless otherwise defined in this Privacy Policy ", textAlign: TextAlign.justify, style: TextStyle(fontSize: 16, fontWeight:FontWeight.w400),),
+              SizedBox(height :8),
+              Text("Information Collection and Use", style:TextStyle(fontSize:24, fontWeight: FontWeight.w600),),
+              Text("For a better experience, while using our service, I may require you to provide us with certain personally identifiable information. The information that I request will be retained on your devide and is not collected by me in any way.", textAlign: TextAlign.justify, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400))
+              SizedBox(height : 8),
+              Text("Security", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600)),
+              Text("We value your trust in providing us your personal inforamtion, thus weare striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable , and I cannot guarantee its absolute security.", textAlign: TextAlign.justify, style:TextStyle(fontSize: 16, fontWeight: FontWeight.w400))
+              , SizedBox(height: 8),
+              Text("Changes to privacy policy", style: TextStyle(fontSize:24, fontWeight: FontWeight.w600),),
+              Text("We may update our privavy policy from time to time.Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new privacy policy on this page", textAlign: TextAlign.justify, style: TextStyle(fontSize:16, fontWeight: FontWeight.w400),),
+              SizedBox(height:8),
+              Text("Contact Us", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),),
+              RichText(textAlign: TextAlign.justify, text: TextSpan(
+                text: 'In case of any query, questions or suggestions about our privacy concerns, please do not hesitate to contact us at ', 
+                style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w400, height:1.4),
+                children: [
+                  TextSpan(
+                    text: 'jolieprincesseishimwe@gmail.com',
+                    style: TextStyle(color: primaryColor, fontWeight: FontWeight.bold, fontSize: 20),
+                  )
+                ]
+              ))
+              
+            ],
+          ),
+        ),
+      ),
+    ),
+    );
+  }
+}
---
+++ lib/view/profile.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:electa/components/button.dart';
+import 'package:electa/components/custom_app_bar.dart';
+import 'package:electa/components/label_input_field.dart';
+import 'package:electa/utils/app_colors.dart';
+import 'package:electa/view_model/profile_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController cityController = TextEditingController();
+  TextEditingController genderController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+
+  @override
+
+  void initState(){
+    super.initState();
+
+    final profileProvider = Provider.of<ProfileProvider>(context, listen: false);
+    profileProvider.getProfile().then((_){
+      setState((){
+        if(profileProvider.user !=null){
+          nameController.text = profileProvider.user['name'] ?? '';
+          ageController.text = profileProvider.user['age'].toString();
+          genderController.text = profileProvider.user['gender'] ?? '';
+          cityController.text = profileProvider.user['city'] ?? '';
+          emailController.text = profileProvider.user['email'] ?? '';
+          phoneController.text = profileProvider.user['phone'] ?? '';
+        }
+      });
+    });
+  }
+
+
+  @override
+
+  Widget build(BuildContext context){
+    var profileProvider = Provider.of<ProfileProvider>(context, listen: false);
+    return Scaffold(
+      appBar: customAppBar(
+        title: AppLocalizations.of(context)!.profile,
+        context: context
+      ),
+      body: SingleChildScrollView(
+       child: SafeArea(
+        child: 
+        Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 11.0),
+                    child: Divider(thickness: 3, color: primaryColor),
+                  ),
+                  Container(
+
+                    width:90,
+                    height:90,
+
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color:primaryColor,
+                    ),
+                    child:
+                    Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: FadeInImage.assetNetwork(fit: BoxFit.cover, placeholder: 'assets/images/loading_gif.gif', image: profileProvider.user?['photo'] ?? 'https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg' ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 7),
+
+              Column(
+                children: [
+                  LabelInputField(controller: nameController, label: AppLocalizations.of(context)!.name, disable: true),
+                  LabelInputField(controller: ageController, label: AppLocalizations.of(context)!.age, disable: true),
+                  LabelInputField(controller: genderController, label: AppLocalizations.of(context)!.gender, disable: true),
+                  LabelInputField(controller: cityController, )
+                ]
+              )
+            ]
+          )
+        )
+       ) 
+      )
+    )
+  }
+}
